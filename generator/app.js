60b6aa95b100fccced04bd336ce2184577e9dc96
imranyy <imranmatano6@gmail.com>
2022-06-12 14:15:41 +0300

    done with qr generator

---
+++ generator/app.js
@@ -0,0 +1,23 @@
+const wrapper=document.querySelector('.wrapper');
+generateBtn=wrapper.querySelector('.form button');
+qrInput=wrapper.querySelector('.form input');
+qrImg=wrapper.querySelector('.qr-code img');
+
+generateBtn.addEventListener('click',()=>{
+    //wrapper.classList.add('active')
+    let qrValue=qrInput.value;
+    //console.log(qrValue)
+    if(!qrValue) return;
+    generateBtn.innerText="Generatin Qr Code..."
+    qrImg.src=`https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+    qrImg.addEventListener('load',()=>{
+        wrapper.classList.add('active');
+        generateBtn.innerText="Generate Qr Code";
+    })
+
+});
+qrInput.addEventListener('keyup',()=>{
+    if(!qrInput.value){
+        wrapper.classList.remove('active')
+    }
+});
