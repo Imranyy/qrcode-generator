80d86b213d09989cfb1ea9bcb5e0d5f7636a7455
imranyy <imranmatano6@gmail.com>
2022-06-12 16:37:17 +0300

    changes

--- app.js
+++ app.js
@@ -21,3 +21,9 @@ qrInput.addEventListener('keyup',()=>{
         wrapper.classList.remove('active')
     }
 });
+
+//register service worker
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('./sw.js').then((reg)=>{console.log('sw registered',reg)})
+    .catch((err)=>{console.log('sw not registered',err)})
+}
